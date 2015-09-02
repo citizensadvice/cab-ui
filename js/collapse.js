fc7e74b7a8ee48a2a827bf1fc7727169a29d7c52
designkai <hello@dperri.com>
2015-09-02 17:02:57 +0100

    Created collapse
    - Removed all instanced of ‘accordion’ and replaced with ‘collapse’
    (including file names)
    - Removed collapse dependency on callout
    - Created collapse specific mixing and .less file
    - Updated html headers to use the new collapse.js

---
+++ js/collapse.js
@@ -0,0 +1,21 @@
+(function($) {
+    $.fn.goTo = function() {
+        $('html, body').animate({
+            scrollTop: $(this).offset().top - 25
+        });
+        return this;
+    }
+})(jQuery);
+
+$(document).ready(function($) {
+    $('#collapse').find('.collapse-toggle').click(function(){
+        $(this).toggleClass("open");
+        $(this).next().slideToggle();
+        $(this).goTo();
+    });
+    $('#collapse').find('.collapse-toggle-close').click(function(){
+        $(this).parent().slideToggle();
+        $(this).parent().prev().toggleClass("open");
+        $(this).parent().prev().goTo();
+    });
+});
--- js/min/collapse-min.js
+++ js/min/collapse-min.js
@@ -1,21 +1,22 @@
 (function($) {
     $.fn.goTo = function() {
         $('html, body').animate({
-            scrollTop: $(this).offset().top - 20
+            scrollTop: $(this).offset().top - 25
         });
         return this;
     }
 })(jQuery);
 
 $(document).ready(function($) {
-    $('#accordion').find('.accordion-toggle').click(function(){
+    $('#collapse').find('.collapse-toggle').click(function(){
         $(this).toggleClass("open");
         $(this).next().slideToggle();
         $(this).goTo();
     });
-    $('#accordion').find('.accordion-toggle-close').click(function(){
+    $('#collapse').find('.collapse-toggle-close').click(function(){
         $(this).parent().slideToggle();
         $(this).parent().prev().toggleClass("open");
         $(this).parent().prev().goTo();
     });
 });
+
