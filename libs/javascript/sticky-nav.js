898df69d9d4d27c01fbd7fb0e760b37ab529c61e
designkai <hello@dperri.com>
2015-09-16 13:33:45 +0100

    Created C5 + included npm-ui
    - Added the content for C5 to consumer-nav.html
    - Style adjustments for consumer-nav.html
    - Integrated npm-ui files into git

--- libs/javascript/sticky-nav.js
+++ libs/javascript/sticky-nav.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
             menu = target;
         $target = $(target);
         $('html, body').stop().animate({
-            'scrollTop': $target.offset().top-40
+            'scrollTop': $target.offset().top-55
         }, 500, 'swing', function () {
             window.location.hash = target;
             $(document).on("scroll", onScroll);
