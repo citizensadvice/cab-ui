027aee14f81002498f71795b6c80083df4e57887
designkai <hello@dperri.com>
2015-11-20 14:43:52 +0000

    Sorted out styles and JS

--- libs/javascript/collapse.js
+++ libs/javascript/collapse.js
@@ -23,3 +23,11 @@ $(document).ready(function($) {
         $(this).parent().prev().goTo();
     });
 });
+
+// Collapsible tables
+$(document).ready(function($) {
+    $('.table-collapse--toggle').click(function(){
+        $(this).toggleClass("active");
+        $(this).next().toggle();
+    });
+});
