357e0e8b42bac0b3f3efc23c763aa5cc8ae1091d
Daniel Lewis <github@mrdaniellewis.co.uk>
2015-11-25 12:23:25 +0000

    Complete build process

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,64 @@
  *	Runs the gulp build tasks
  */
 
-// Import dependencies
 var gulp = require( 'gulp' );
 var less = require( 'gulp-less' );
 var sourcemaps = require('gulp-sourcemaps');
+
+// Post CSS plugins
 var postcss = require('gulp-postcss');
+// Minify
 var cssnano = require('cssnano');
+// Add a px fallback for rem for IE8
 var pixrem = require('pixrem');
+// Add a hex fallback for rgba for IE8
+var rgbaFallback = require('postcss-color-rgba-fallback');
+// Add an opacity fallback for IE8
+var opacity = require('postcss-opacity');
+// Automatically add/remove required/unrequired prefixes
+var autoprefixer = require('autoprefixer');
+// Copy referenced assets to a folder
+var copyAssets = require('postcss-copy-assets');
+// Add version to the asset based on the updated date.  This allows us to use far future caching on the asset
 var cachebuster = require('postcss-cachebuster');
 
-// Define a task to turn the less into CSS
+var productionBuild = false;
+
+gulp.task( 'production', function() {
+	// This task just sets a flag to say we are in production mode
+	productionBuild = true;
+} );
+
 gulp.task( 'less', function() {
+	var postCssPlugins = [
+		autoprefixer({ browsers: ['> 1%','ie >= 8'] }), 
+		rgbaFallback,
+		opacity,
+		pixrem, 
+		copyAssets, 
+		cachebuster
+	];
+
+	// Only include cssnano on a production build as it takes ages
+	if ( productionBuild ) {
+		postCssPlugins.push(cssnano);
+	}
+
     return gulp.src('libs/*.less')
 		.pipe( sourcemaps.init() ) // Generate sourcemaps
 		.pipe( less() ) // Transform to less
-		.pipe( postcss( [/*cssnano,*/pixrem,cachebuster] ) )
+		.pipe( postcss( postCssPlugins, { to: 'public/css/assets' } ) ) // Postcss (the "to" is for copyAssets)
 		.pipe( sourcemaps.write('.') ) // Write the sourcemaps
 		.pipe( gulp.dest( 'public/css') ); // Write the less
 });
 
 // Watch everything in the static-src folder and rerun default if it changes
 gulp.task('watch-less', function () {
-    gulp.watch('static-src/**', ['default']);
+    gulp.watch('libs/**', ['default']);
 });
 
 gulp.task('default', ['less']);
+// These tasks are actually run in parallel, but as production will be called and run first
+gulp.task('build', ['production','less'] );
 gulp.task('watch', ['default', 'watch-less']);
 
